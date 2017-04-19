dacd29465204956b690bfd2034debeac55c2928c
jeka <ElectroDeggi33759@yandex.ru>
2017-04-19 21:00:09 +0300

    1

--- app/js/common.js
+++ app/js/common.js
@@ -113,3 +113,15 @@ $(function() {
 		return false;
 	})
 });
+
+
+
+$(document).ready(function() {
+	$('#adaptive').lightSlider({
+		adaptiveHeight:true,
+		item:1,
+		slideMargin:0,
+		loop:true,
+		controls:false
+	});
+});
